be8514fbbc0855fe1e8f18bb50d5cc807d321674
Patrick Hulce <patrick.hulce@gmail.com>
2016-11-14 22:10:28 -0800

    fix: correct binary behavior

--- bin/dcraw.js
+++ bin/dcraw.js
@@ -1,6 +1,10 @@
+#!/usr/bin/env node
+
 require('../lib/run')(process.argv.slice(2)).
   then(output => console.log(output)).
   catch(err => {
-    console.log(err.stack);
     console.log(err.output);
+    if (!err.output && !err.output.match(/Usage:/g)) {
+      console.log(err.stack);
+    }
   });
